a55e487cb5a36088010e3ccc6cc174d1f141b9a3
MaximN <nashdomik27+gh@gmail.com>
2026-08-04 12:23:49 -0400

    intake code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/intake.java
@@ -6,33 +6,56 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.subsystems.shooter.ShooterConstants;
 
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class intake {
+public class intake extends SubsystemBase {
 
     private final Telemetry telemetry;
     private final DcMotorEx intake;
-    private DoubleSupplier voltage;
+    private final DoubleSupplier voltage;
     private double kSetpoint;
-    private PIDController veloPID;
-
+    private final PIDController veloPID;
 
     public intake(HardwareMap hwMap, Telemetry telemetry) {
         this.telemetry = telemetry;
 
-        intake = (hwMap.get(DcMotorEx.class, "shooter"));
-
+        intake = hwMap.get(DcMotorEx.class, "cm2");
         this.voltage = () -> hwMap.voltageSensor.iterator().next().getVoltage();
 
         intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
         intake.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        kSetpoint = ShooterConstants.setpoint;
-        veloPID = new PIDController(ShooterConstants.kp, ShooterConstants.ki, ShooterConstants.kd);
-        veloPID.setTolerance(10);
-    }}
+        veloPID = new PIDController(0.0, 0.0, 0.0); // tune these
+    }
+
+    public void setPower(double power) {
+        intake.setPower(1);
+    }
+
+    public void setTargetVelocity(double ticksPerSecond) {
+        kSetpoint = ticksPerSecond;
+    }
+
+    public double getVelocity() {
+        return intake.getVelocity();
+    }
+
+    public void stop() {
+        intake.setPower(0);
+    }
+
+    @Override
+    public void periodic() {
+        if (kSetpoint != 0) {
+            double output = veloPID.calculate(getVelocity(), kSetpoint);
+            // optional voltage compensation:
+            // output *= 12.0 / voltage.getAsDouble();
+            intake.setPower(output);
+        }
+        telemetry.addData("Intake Velocity", getVelocity());
+    }
+}

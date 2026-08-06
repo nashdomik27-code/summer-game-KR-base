35390baec34dcf9627682acfceaf896cff9332cb
MaximN <nashdomik27+gh@gmail.com>
2026-08-06 11:41:09 -0400

    Constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/conveyor/ConveyorConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.subsystems.conveyor;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class ConveyorConstants {
+    public static double kp=0,ki=0,kd=0,kv=0;
+    public static double setpoint = 0;
+    public static double drop = 1020;
+    public static double idle = 0;
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/intakeConstants.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.subsystems.intake;
+
+import com.bylazar.configurables.annotations.Configurable;
+
+@Configurable
+public class intakeConstants {
+    public static double kp=0,ki=0,kd=0,kv=0;
+    public static double setpoint = 0;
+    public static double wheels = 1020;
+    public static double idle = 0;
+}
